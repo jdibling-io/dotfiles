7066cdcc219e71d6e9896546b97d4cea5278ed78
jdibling <john.dibling@stream-ai.io>
2023-02-18 12:01:30 +0000

    update chezmoi install

--- install.sh
+++ install.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-cd /home/vscode
-sh -c "$(wget -qO- get.chezmoi.io)"
-sudo ln -s ~/bin/chezmoi /usr/local/bin/chezmoi
-cd -
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+sudo ln -s $HOME/.local/bin/chezmoi /usr/local/bin/chezmoi
 
 chezmoi init https://github.com/jdibling-io/dotfiles.git
 chezmoi apply -v
