6e188f1c535e0c9e052be23725f5d970e5db08e9
jdibling <john.dibling@stream-ai.io>
2023-02-18 12:30:38 +0000

    install firacode

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ set -e
 sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
 sudo ln -s $HOME/.local/bin/chezmoi /usr/local/bin/chezmoi
 
+sudo apt install -y fonts-firacode
+
 chezmoi init https://github.com/jdibling-io/dotfiles.git
 chezmoi apply -v
 
